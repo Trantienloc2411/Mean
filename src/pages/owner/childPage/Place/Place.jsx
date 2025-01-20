32557a875dab47cba8730dfca261d087d85a006d
pion_shariff <62489239+Trantienloc2411@users.noreply.github.com>
2025-01-21 01:49:40 +0700

    Add screen and fix minor bug from other screen

--- src/pages/owner/childPage/Place/Place.jsx
+++ src/pages/owner/childPage/Place/Place.jsx
@@ -1,4 +1,26 @@
-import styles from '../Place/Place.module.scss';
-export default function Place(props) { 
-    return <div>Place Page</div>
+import { Button, Tooltip } from "antd";
+import styles from "../Place/Place.module.scss";
+import { useNavigate } from "react-router-dom";
+export default function Place(props) {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    navigate("./AccomodationDetail");
+  };
+
+
+  const handleCreateNavigate = () => {
+    navigate("./AccomodationCreate");
+  }
+  return (
+    <>
+      <Tooltip title="CurrentPage">
+        <Button onClick={handleNavigate}>DetailPage</Button>
+      </Tooltip>
+
+      <Tooltip title="CurrentPage">
+        <Button onClick={handleCreateNavigate}>CreatePage</Button>
+      </Tooltip>
+    </>
+  );
 }
