5b30f38765859d7946959161e1186eedf8074afe
Zanef08 <nhiphm302@gmail.com>
2025-02-25 15:29:00 +0700

    [Zane] Done api for Service

--- src/pages/owner/childPage/TypeRoom/components/RoomAmenitiesManagement/components/DeleteAmenityModal/DeleteAmenityModal.jsx
+++ src/pages/owner/childPage/TypeRoom/components/RoomAmenitiesManagement/components/DeleteAmenityModal/DeleteAmenityModal.jsx
@@ -1,27 +1,35 @@
-import { Modal, Button } from 'antd';
+import { Modal, Button, Spin } from 'antd';
 import styles from './DeleteAmenityModal.module.scss';
 
-const DeleteAmenityModal = ({ isOpen, onCancel, onConfirm, amenityName }) => {
+const DeleteAmenityModal = ({ isOpen, onCancel, onConfirm, amenityName, isLoading }) => {
   return (
     <Modal
       title="Xác nhận xoá tiện ích"
       open={isOpen}
       onCancel={onCancel}
       footer={[
-        <Button key="cancel" onClick={onCancel} className={styles["ant-btn"]}>
+        <Button key="cancel" onClick={onCancel} className={styles["ant-btn"]} disabled={isLoading}>
           Huỷ
         </Button>,
-        <Button key="delete" danger type="primary" onClick={onConfirm} className={styles["ant-btn-danger"]}>
+        <Button 
+          key="delete" 
+          danger 
+          type="primary" 
+          onClick={onConfirm} 
+          className={styles["ant-btn-danger"]}
+          loading={isLoading}
+        >
           Có, Tôi chắc chắn
         </Button>
       ]}
       className={styles["modal-container"]}
     >
-      <p>
-        Bạn đang sắp xoá tiện ích "{amenityName}". Hành động này không thể hoàn tác.
-      </p>
+      <Spin spinning={isLoading}>
+        <p>
+          Bạn đang sắp xoá tiện ích "{amenityName}". Hành động này không thể hoàn tác.
+        </p>
+      </Spin>
     </Modal>
-
   );
 };
 
